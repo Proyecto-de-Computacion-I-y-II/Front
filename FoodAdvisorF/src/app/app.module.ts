abad1b40698878bfdaa9bb4eca469ddf51fe2896
Jachpi <121670083+Jachpi@users.noreply.github.com>
2025-04-04 14:01:04 +0200

    gestión de redirección de registro

--- FoodAdvisorF/src/app/app.module.ts
+++ FoodAdvisorF/src/app/app.module.ts
@@ -1,27 +1,27 @@
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { MatSlider, MatSliderRangeThumb } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatCardModule } from '@angular/material/card';
-import { HomeComponent } from './core/home/home.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { LoginComponent } from './security/login/login.component';
-import { MatIconModule } from '@angular/material/icon';
-import {MatFormField} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatOption, MatSelect} from '@angular/material/select';
-import {MatSlider, MatSliderRangeThumb} from '@angular/material/slider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {provideHttpClient} from '@angular/common/http';
-import {RegisterComponent} from './security/register/register.component';
-import {MatButtonToggleGroup} from '@angular/material/button-toggle';
-import { ProfileComponent } from './core/profile/profile.component';
 import { CestaComponent } from './core/cesta/cesta.component';
+import { HomeComponent } from './core/home/home.component';
+import { ProfileComponent } from './core/profile/profile.component';
 import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
+import { LoginComponent } from './security/login/login.component';
+import { RegisterComponent } from './security/register/register.component';
+import { AuthInterceptor } from './shared/services/interceptor/auth.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -51,8 +51,12 @@ import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.co
     ProductoDetalleComponent
   ],
   providers: [
-    provideHttpClient()
-
+    provideHttpClient(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
