361dac8aeb13d6d868482ba7a453c4b5c9f566a1
Jachpi <121670083+Jachpi@users.noreply.github.com>
2025-04-09 22:58:52 +0200

    inicio (no responsive)

--- FoodAdvisorF/src/app/app.module.ts
+++ FoodAdvisorF/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.co
 import { LoginComponent } from './security/login/login.component';
 import { RegisterComponent } from './security/register/register.component';
 import { CestasComponent } from './core/cestas/cestas.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { CestasComponent } from './core/cestas/cestas.component';
     RegisterComponent,
     ProfileComponent,
     CestaComponent,
-    CestasComponent
+    CestasComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
